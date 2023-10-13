acf6f7ebea5af3c852947bf5ffce1380846fc4e6
m-sgstyb <108869912+m-sgstyb@users.noreply.github.com>
2023-10-13 16:00:50 +0100

    Added code to extract asset costs and sizes

---
+++ Code/Results/GMPA_costs_sizes.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 13 15:28:01 2023
+
+@author: mnsgs
+"""
+
+import pandas as pd
+
+def export_asset_costs(my_network):
+    costs_df = pd.DataFrame()
+
+    ### RE assets ###
+    costs_df.insert(0, 'PV_cost_G$', [my_network.assets[0].cost.value])
+    costs_df.insert(0, 'Wind_cost_G$', [my_network.assets[1].cost.value])
+   
+    
+    ### BESS ###
+    costs_df.insert(0, 'BESS_G$', [my_network.assets[2].cost.value])
+
+    
+    ### NH3 to electricity, storage and electricity to NH3 ###
+    costs_df.insert(0, 'NH3_to_EL_G$', [my_network.assets[4].cost.value])
+    costs_df.insert(0, 'NH3_storage_G$', [my_network.assets[5].cost.value])
+    costs_df.insert(0, 'EL_to_NH3_G$', [my_network.assets[6].cost.value])
+    
+    ### NH3 to HTH ###
+    costs_df.insert(0, 'NH3_to_HTH_G$', [my_network.assets[7].cost.value])
+    
+    
+    ### EL to HTH ###
+    costs_df.insert(0, 'EL_to_HTH_G$', [my_network.assets[9].cost.value])
+    
+    
+    ### PowerPlant CO2 ###
+    costs_df.insert(0, 'PP_CO2_G$', [my_network.assets[11].cost.value])
+    
+    ### Demands ###
+    costs_df.insert(0, 'HTH_demand_G$', [my_network.assets[8].cost.value])
+    costs_df.insert(0, 'EL_demand_G$', [my_network.assets[3].cost.value])
+    
+    ### Total cost ### 
+    costs_df.insert(0, 'Total_System_Cost', [my_network.problem.value])
+    
+    return(costs_df)
+
+
+def export_asset_sizes(my_network):
+    
+    sizes_df = pd.DataFrame()
+
+    ### RE assets ###
+    sizes_df.insert(0, 'PV_size_GWp', [my_network.assets[0].asset_size()])
+    sizes_df.insert(0, 'Wind_size_GWp', [my_network.assets[1].asset_size()])
+    
+    ### BESS ###
+    sizes_df.insert(0, 'BESS_GWh', [my_network.assets[2].asset_size()])
+    
+    ### NH3 to electricity, storage and electricity to NH3 ###
+    sizes_df.insert(0, 'NH3_to_EL_G$', [my_network.assets[4].asset_size()])
+    sizes_df.insert(0, 'NH3_storage_G$', [my_network.assets[5].asset_size()])
+    sizes_df.insert(0, 'EL_to_NH3_G$', [my_network.assets[6].asset_size()])
+    
+    ### NH3 to HTH ###
+    sizes_df.insert(0, 'NH3_to_HTH_G$', [my_network.assets[7].asset_size()])
+    
+    
+    ### EL to HTH ###
+    sizes_df.insert(0, 'EL_to_HTH_G$', [my_network.assets[9].asset_size()])
+    
+    
+    ### PowerPlant CO2 ###
+    sizes_df.insert(0, 'PP_CO2_G$', [my_network.assets[11].asset_size()])
+    
+    ### Demands ###
+    sizes_df.insert(0, 'HTH_demand_G$', [my_network.assets[8].asset_size()])
+    sizes_df.insert(0, 'EL_demand_G$', [my_network.assets[3].asset_size()])
+    
+    ### Emissions ###
+    sizes_df.insert(0, 'CO2_Budget', [my_network.assets[10].asset_size()])
+    
+    return(sizes_df)
--- main.py
+++ main.py
@@ -14,11 +14,11 @@ import cvxpy as cp
 
 from Code.Network.Network import Network_STEVFNs
 from Code.Plotting import DPhil_Plotting
-
+from Code.Results import GMPA_costs_sizes
 
 
 #### Define Input Files ####
-case_study_name = "EM_Case_Study"
+case_study_name = "Autarky"
 
 
 base_folder = os.path.dirname(__file__)
@@ -88,14 +88,9 @@ for counter1 in range(len(scenario_folders_list)):
     # DPhil_Plotting.plot_asset_sizes(my_network)
     DPhil_Plotting.plot_asset_costs(my_network)
     
-# ##### Plot flows for BAU scenario ######
-# DPhil_Plotting.plot_SG_EL_input_flows_BAU(my_network)
-# DPhil_Plotting.plot_SG_EL_output_flows_BAU(my_network)
-# DPhil_Plotting.plot_RE_EL_input_flows_BAU(my_network)
-# DPhil_Plotting.plot_RE_EL_output_flows_BAU(my_network)
-
-# DPhil_Plotting.plot_SG_NH3_input_flows_BAU(my_network)
-# DPhil_Plotting.plot_SG_NH3_output_flows_BAU(my_network)
-# DPhil_Plotting.plot_RE_NH3_input_flows_BAU(my_network)
-# DPhil_Plotting.plot_RE_NH3_output_flows_BAU(my_network)
+    
+    
+    ### Get results for GMPA ###
+    
+    GMPA_costs_sizes.export_asset_costs(my_network).to_csv(f'{scenario_folders_list[counter1]}_Costs.csv', index = False, header=True)
 
