70ebf68054e580109663545c7a7c1ff2abe785e9
Monica Sagastuy <monica.sagastuybrena@eng.ox.ac.uk>
2023-10-31 12:41:36 +0000

    CO2 budget parameters for SGP-IDN-MYS collaboration after running BAU

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ import cvxpy as cp
 
 from Code.Network.Network import Network_STEVFNs
 from Code.Plotting import DPhil_Plotting
-
+from Code.Results import GMPA_costs_sizes_three_country
 
 
 #### Define Input Files ####
-case_study_name = "EM_Case_Study"
+case_study_name = "Three_Country_Collab_0_1_2"
 
 
 base_folder = os.path.dirname(__file__)
@@ -88,14 +88,18 @@ for counter1 in range(len(scenario_folders_list)):
     # DPhil_Plotting.plot_asset_sizes(my_network)
     DPhil_Plotting.plot_asset_costs(my_network)
     
-# ##### Plot flows for BAU scenario ######
-# DPhil_Plotting.plot_SG_EL_input_flows_BAU(my_network)
-# DPhil_Plotting.plot_SG_EL_output_flows_BAU(my_network)
-# DPhil_Plotting.plot_RE_EL_input_flows_BAU(my_network)
-# DPhil_Plotting.plot_RE_EL_output_flows_BAU(my_network)
 
-# DPhil_Plotting.plot_SG_NH3_input_flows_BAU(my_network)
-# DPhil_Plotting.plot_SG_NH3_output_flows_BAU(my_network)
-# DPhil_Plotting.plot_RE_NH3_input_flows_BAU(my_network)
-# DPhil_Plotting.plot_RE_NH3_output_flows_BAU(my_network)
+
+# Export cost results to csv file
+
+    GMPA_costs_sizes_three_country.export_results(my_network).to_csv(f'{scenario_folders_list[counter1]}_Results.csv', index = False, header=True)
+   
+    # if case_study_name == "Two_Countries_Collab":
+    #     GMPA_costs_sizes_three_country.export_asset_costs_three_country(my_network).to_csv(f'{scenario_folders_list[counter1]}_Costs.csv', index = False, header=True)
+    #     GMPA_costs_sizes_three_country.export_asset_sizes_three_country(my_network).to_csv(f'{scenario_folders_list[counter1]}_Sizes.csv', index = False, header=True)
+    # elif case_study_name == "Two_Countries_Aut":
+    #     GMPA_costs_sizes_three_country.export_asset_costs_three_country_aut(my_network).to_csv(f'{scenario_folders_list[counter1]}_Costs.csv', index = False, header=True)
+    #     GMPA_costs_sizes_three_country.export_asset_costs_three_country_aut(my_network).to_csv(f'{scenario_folders_list[counter1]}_Sizes.csv', index = False, header=True)
+    
+    
 
