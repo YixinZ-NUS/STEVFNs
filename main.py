ff12e96596a20506c3d531672d8057353430f07f
Monica Sagastuy <monica.sagastuybrena@eng.ox.ac.uk>
2023-11-03 21:07:10 +0000

    Added new powerplant assets and included them in dictionary. Added parameters for demand assets and wind assets; updated main and GMPA_Results for total_data format result extraction

--- main.py
+++ main.py
@@ -17,11 +17,28 @@ from Code.Plotting import DPhil_Plotting
 from Code.Results import GMPA_Results
 
 
-
 #### Define Input Files ####
-case_study_name = "Four_Country_Collab"
+# case_study_name = "EM_Case_Study"
 
-# case_study_name = "Four_Country_Aut"
+
+###### Autarky Case Studies #########
+case_study_name = "Autarky_SG"
+# case_study_name = "Autarky_ID"
+# case_study_name = "Autarky_MY"
+
+###### Two Country Case Studies #########
+# case_study_name = "SG-ID_Autarky"
+# case_study_name = "SG-ID_Collab"
+
+# case_study_name = "SG-MY_Autarky"
+# case_study_name = "SG-MY_Collab"
+
+# case_study_name = "ID-MY_Autarky"
+# case_study_name = "ID-MY_Collab"
+
+###### Three Country Case Studies #########
+# case_study_name = "SG-ID-MY_Autarky"
+# case_study_name = "SG-ID-MY_Collab"
 
 
 base_folder = os.path.dirname(__file__)
@@ -29,8 +46,6 @@ data_folder = os.path.join(base_folder, "Data")
 case_study_folder = os.path.join(data_folder, "Case_Study", case_study_name)
 scenario_folders_list = [x[0] for x in os.walk(case_study_folder)][1:]
 network_structure_filename = os.path.join(case_study_folder, "Network_Structure.csv")
-
-
 
 
 ### Read Input Files ###
@@ -54,7 +69,7 @@ print("Time taken to build network = ", end_time - start_time, "s")
 for counter1 in range(len(scenario_folders_list)):
 # for counter1 in range(1):
     ### Read Input Files ###
-    scenario_folder = scenario_folders_list[counter1]
+    scenario_folder = scenario_folders_list[-1-counter1]
     asset_parameters_filename = os.path.join(scenario_folder, "Asset_Parameters.csv")
     location_parameters_filename = os.path.join(scenario_folder, "Location_Parameters.csv")
     system_parameters_filename = os.path.join(scenario_folder, "System_Parameters.csv")
@@ -87,11 +102,26 @@ for counter1 in range(len(scenario_folders_list)):
     ### Plot Results ############
     print("Time taken to solve problem = ", end_time - start_time, "s")
     print("Total cost to satisfy all demand = ", my_network.problem.value, " Billion USD")
+    if my_network.problem.value == float("inf"):
+        break
+    
     # DPhil_Plotting.plot_all(my_network)
     # DPhil_Plotting.plot_asset_sizes(my_network)
-    DPhil_Plotting.plot_asset_costs(my_network)
+    # DPhil_Plotting.plot_asset_costs(my_network)
+    
+    # Export cost results to pandas dataframe
+
+    
+    t_df = GMPA_Results.export_total_data(my_network, location_parameters_df, asset_parameters_df)
+    if counter1 == 0:
+        total_df = t_df
+    else:
+        total_df = pd.concat([total_df, t_df], ignore_index=True)
 
 
-    # Export cost results to csv file
 
-    GMPA_Results.export_results(my_network).to_csv(f'{scenario_folders_list[counter1]}_Results.csv', index = False, header=True)
+#### Save Results
+total_df.to_csv(results_filename, index=False, header=True)
+
+   
+   
